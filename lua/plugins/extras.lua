9553e0ca6f0fb6ba15d3ad460f193ed6cdcfdf15
Nivekithan S <nivekithan363@gmail.com>
2023-02-05 00:14:47 +0530

    Init nvim config

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,10 @@
+return {
+
+  { "nvim-neo-tree/neo-tree.nvim", opts = { window = { position = "right" } } },
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
+  -- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
+  { import = "lazyvim.plugins.extras.lang.json" },
+}
